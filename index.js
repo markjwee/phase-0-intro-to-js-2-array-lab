e9da1e8d37d640738186f85fe1df75d90b3a5676
Mark <mark.j.wee@gmail.com>
2021-12-02 19:32:44 -0500

    upload for TC help

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(...name) {
+    return cats.slice(2,2)
+}
